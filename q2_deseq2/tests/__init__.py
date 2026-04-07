7a302a976a8c9a06493d7fd3e34aa5385e776ab1
Michal Ziemski <mziemski@ethz.ch>
2026-04-07 18:19:18 +0200

    CI: add conda recipe

--- q2_deseq2/tests/__init__.py
+++ q2_deseq2/tests/__init__.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 # Copyright (c) 2026, QIIME 2 development team.
 #
-# Distributed under the terms of the Modified BSD License.
+# Distributed under the terms of the Lesser General Public License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
